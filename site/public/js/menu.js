e9afdd4ecef104574a1e52b8cc069b7d36f53701
RobertoTurbiani <roberto.tfariasjr@sptech.school>
2023-05-10 09:22:02 -0300

    Api ligada

---
+++ site/public/js/menu.js
@@ -0,0 +1,13 @@
+function nav_open() {
+        
+    headerMenu.style.display ='block'
+    div_close.style.display = 'block'
+    div_open.style.display = 'none'
+    
+}
+
+function nav_close() {
+    headerMenu.style.display ='none'
+    div_open.style.display = 'block'
+    div_close.style.display = 'none'
+}
