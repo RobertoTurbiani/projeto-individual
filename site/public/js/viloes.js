ec2d1202c6a1ae62ce5c657305247467da94151c
RobertoTurbiani <roberto.tfariasjr@sptech.school>
2023-05-11 00:04:28 -0300

    json dos viloes

---
+++ site/public/js/viloes.js
@@ -0,0 +1,132 @@
+    var rhino = {
+    nome: "Rhino",
+    hp: "150",
+    att: "70",
+    def: "90",
+    }
+
+    var abutre = {
+    nome: "Abutre",
+    hp: "90",
+    att: "60",
+    def: "30"
+    }
+    var scorpion = {
+    nome: "Scorpion",
+    hp: "120",
+    att: "65",
+    def: "50"
+    }
+    var electro = {
+    nome: "Electro",
+    hp: "100",
+    att: "70",
+    def: "40"
+    }
+    var shocker = {
+    nome: "Shocker",
+    hp: "110",
+    att: "60",
+    def: "40"
+        }
+    var misterio = {
+    nome: "Misterio",
+    hp: "80",
+    att: "50",
+    def: "90"
+    }
+        
+    var lagarto = {         
+    nome: "Lagarto",
+    hp: "130",
+    att: "90",
+    def: "80",
+    }
+    var duendeVerde = {
+    nome: "Duende Verde",
+    hp: "130",
+    att: "80",
+    def: "80",
+    }
+    var duendeMacabro = {
+    nome: "Duende Macabro",
+    hp: "120",
+    att: "70",
+    def: "90",
+    }
+    var venom = {
+    nome: "Venom",
+    hp: "130",
+    att: "100",
+    def: "100",
+    }
+    var carnage = {
+    nome: "Carnage",
+    hp: "120",
+    att: "110",
+    def: "110",
+    }
+                            
+    var silverSable = {               
+    nome: "Silver Sable",
+    hp: "70",
+    att: "40",
+    def: "60",
+    }
+    var reiDoCrime = {       
+    nome: "Rei do Crime",
+    hp: "60",
+    att: "70",
+    def: "80",
+    }
+    var cabecaDeMartelo = {      
+    nome: "Cabeça de Martelo",
+    hp: "90",
+    att: "80",
+    def: "80",
+    }
+    var gataNegra = {                                       
+    nome: "gata Negra",
+    hp: "60",
+    att: "40",
+    def: "70",
+    }
+    var morbius = {
+    nome: "Morbius",
+    hp: "110",
+    att: "80",
+    def: "100",
+    }
+                                                
+    var morlun = {               
+    nome: "Morlun",
+    hp: "100",
+    att: "80",
+    def: "90",
+    }
+    var kraven = {       
+    nome: "Kraven",
+    hp: "120",
+    att: "90",
+    def: "80",
+    }
+    
+    var drOctopus = {                
+    nome: "Dr. Octopus",
+    hp: "130",
+    att: "120",
+    def: "90" ,                                                                          }
+
+    var srNegativo = {
+    nome: "Sr. Negativo",
+    hp: "90",
+    att: "80",
+    def: "60",
+    }
+
+    var homemAreia = {
+    nome: "Homem Areia",
+    hp: "120",
+    att: "80",
+    def: "120",
+    }
