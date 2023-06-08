cd9f38df630b738c1b25ed3ce68fd76f76b124eb
Roberto Turbiani <roberto.tfariasjr@sptech.school>
2023-06-08 09:33:09 -0300

    organização

--- site/app.js
+++ site/app.js
@@ -13,7 +13,6 @@ var usuarioRouter = require("./src/routes/usuarios");
 var avisosRouter = require("./src/routes/avisos");
 var medidasRouter = require("./src/routes/medidas");
 var personagemRouter = require("./src/routes/personagem");
-var votacaoRouter = require("./src/routes/votacao");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,7 +25,6 @@ app.use("/usuarios", usuarioRouter);
 app.use("/avisos", avisosRouter);
 app.use("/medidas", medidasRouter);
 app.use("/personagem", personagemRouter);
-app.use("/votacao", votacaoRouter);
 
 app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
--- site/src/database/script-Hero.sql
+++ site/src/database/script-Hero.sql
@@ -11,21 +11,6 @@ defense INT NOT NULL,
 hero boolean,
 url varchar(255));
 
-select * from usuario;	
-
-CREATE TABLE votos_uniforme (
-  idUniforme INT PRIMARY KEY AUTO_INCREMENT,
-  traje VARCHAR(50) NOT NULL,
-  votos INT DEFAULT 0
-);
-
-CREATE TABLE votos_personagem (
-  idPersonagem INT PRIMARY KEY AUTO_INCREMENT,
-  personagem VARCHAR(50) NOT NULL,
-  votos INT DEFAULT 0
-);
-
-
 create table usuario (
 idUsuario int primary key auto_increment,
 nome varchar (50),
@@ -33,37 +18,31 @@ email varchar (45),
 senha varchar (40),
 vitoria int,
 derrota int,
-empate int,
-fkUniforme int,
-constraint fkUniforme foreign key (fkUniforme) 
-	references votos_uniforme(idUniforme),
-fkPersonagem int,
-constraint fkPersonagem foreign key (fkPersonagem)
-	references votos_personagem (idPersonagem)
+empate int
 );
 
-
-
-insert into votos_uniforme values
-(null,'Noir', null),
-(null,'2099',null),
-(null,'Simbionte',null),
-(null,'Traje Classico',null),
-(null,'Aranha de Ferro',null);
-
-insert into votos_personagem values
-	(null,'Venom',null),
-	(null,'Spider-Gwen',null),
-	(null,'Spider-Man',null),
-	(null,'Miles Morales',null),
-	(null,'2099',null),
-	(null,'Ultimate-Spider-Man',null),
-	(null,'Gata Negra',null);
-
-
+CREATE TABLE votos_uniforme (
+  traje VARCHAR(50),
+  votos int,
+  fkUsuario int,
+  constraint fkUsuario foreign key (fkUsuario)references usuario (idUsuario),
+  constraint votos primary key (traje,fkUsuario)
+);
+    
+CREATE TABLE votos_personagem (
+  personagem VARCHAR(50),
+  votos int,
+  fkUser int,
+  constraint fkUser foreign key (fkUser)references usuario (idUsuario),
+  constraint votos primary key (personagem,fkUser)
+);
 
 select * from usuario;
 select * from personagens;
 select * from votos_uniforme;
+select * from votos_personagem;
+
+select count(votos) from  votos_uniforme;
+SELECT count(votos), max(votos), min(votos),truncate(avg(votos),0) FROM votos_uniforme;
 
 desc personagens;
